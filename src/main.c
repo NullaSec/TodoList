086376b9077bc6e9ee4cbe06d27e5a3f99390efe
nullasec <jafonsonm@protonmail.com>
2026-04-22 23:24:42 +0100

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    printf("Welcome to Zeca's TODO List!\n");
+    printf("============================\n");
+    printf("1: View Active Tasks\n");
+    printf("2: Add Task\n");
+    printf("5: Exit\n");
+
+    int option;
+    printf("Choose a number: \n");
+    scanf("%d", &option);
+
+
+    return 0;
+}
